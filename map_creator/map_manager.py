b87b91de62669ab753ae09c8b0cecb7899c1c71a
skillor <jack-dark1@gmx.de>
2020-11-11 11:28:18 +0100

    fixed saving

--- map_creator/map_manager.py
+++ map_creator/map_manager.py
@@ -14,15 +14,17 @@ class MapManager:
             path = os.path.join(folder, filename, 'inner-world.json')
             if os.path.isfile(path):
                 self.maps[filename] = path
+        self.current_map = None
         self.select_map('world')
 
     def save(self):
-        self.current_map.save()
+        if self.current_map is not None:
+            self.current_map.save()
 
     def toggle_grid(self):
         self.current_map.toggle_grid()
 
     def select_map(self, name):
         self.save()
-        self.selected_map = self.maps[name]
-        self.current_map = Map(self.tm, self.om, self.trm, self, self.selected_map)
+        selected_map = self.maps[name]
+        self.current_map = Map(self.tm, self.om, self.trm, self, selected_map)
